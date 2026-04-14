fc789b9b4cc28a40233c5ce79beee575814de96c
sahil095 <sehgal.sahil0786@gmail.com>
2026-04-14 00:25:59 -0700

    database added

--- schemas.py
+++ schemas.py
@@ -1,4 +1,22 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+from datetime import datetime
+
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50, example="johndoe")
+    email: EmailStr = Field(max_length=120)
+
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id: int
+    image_file: str | None
+    image_path: str
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100, example="My First Post")
